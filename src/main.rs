3e12e0f33d5a0fca073df270c42933cba79a633a
R <jugle.orgx99@gmail.com>
2024-05-10 19:24:10 +0100

    add files

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::time::Duration;
+use anyhow::{Context, Result};
+use rand::{Rng, thread_rng};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // get the two arguments
+    let args: Vec<String> = std::env::args().collect();
+    let start_arg = "42".to_string();
+    let end_arg = "1337".to_string();
+    let start = args.get(0).unwrap_or(&start_arg);
+    let end = args.get(1).unwrap_or(&end_arg);
+
+    let mut ban_count = 0;
+    for _ in 0..5 {
+        // Command
+        let mut sum = 0;
+        for i in 0..4 {
+            let mut rng = thread_rng();
+            
+            // Generate a random integer between 1 and 10
+            let coin: i8 = rng.gen_range(1..=2);
+            sum += coin
+        }
+
+        println!("your sum is {}", sum);
+        ban_count += if sum % 2 == 0 { 1 } else { 0 };
+    }
+    println!("you are...");
+    tokio::time::sleep(Duration::from_secs(3)).await;
+    println!("ban_count {}", ban_count);
+    return Ok(());
+}
