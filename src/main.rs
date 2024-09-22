ab377ecc3aafb43d677949013ca42fad9465f28d
R <jugle.orgx99@gmail.com>
2024-09-22 19:39:06 +0100

    remove rust ui

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
-use std::{time::Duration, io, io::Write};
-use anyhow::Result;
-use rand::{Rng, thread_rng};
-use dialoguer::Select;
+use {
+    anyhow::Result, clap::Parser, rand::{thread_rng, Rng}, std::time::Duration
+};
+
+enum Face {
+    Heads,
+    Tails,
+}
+impl Face {
+    fn new() -> (Self, u16) {
+        let args = Args::parse();
+        let chr: &str = &args.face.to_lowercase()[..1];
+        println!("{}",chr);
+        let face: Face = match chr {
+            "h" => Face::Heads,
+            "t" => Face::Tails,
+            _ => panic!("heads or tails"),
+        };
+        return (face, args.of)
+    }
+    fn ball(&self) -> &str {
+        return match self {
+            Self::Heads => "Heads",
+            Self::Tails => "Tails",
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    face: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 5)]
+    of: u16,
+}
+
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let faces = &["Heads", "Tails"];
-    let value = Select::new().default(0).items(&faces[..]).interact().unwrap();
-    // get the two arguments
-    let args: Vec<String> = cargo();
-    let start_arg = "42".to_string();
-    let end_arg = "1337".to_string();
-    let start = args.get(0).unwrap_or(&start_arg);
-    let end = args.get(1).unwrap_or(&end_arg);
+    let (face, of) = Face::new();
 
-    let of: u16 = prompt();
     let mut sum: u16 = 0;
     for _ in 0..of {
         let mut rng = thread_rng();
@@ -25,26 +54,6 @@ async fn main() -> Result<()> {
     let percent: f64 = 100.0*f64::from(sum)/f64::from(of);
     println!("drum roll please...");
     tokio::time::sleep(Duration::from_secs(3)).await;
-    println!("{}% {}, you {}", percent, if value == 0 { "Heads" } else { "Tails" }, if percent > 50.0 { "won" } else { "lost" });
+    println!("{}% {}, you {}", percent, face.ball(), if percent > 50.0 { "won" } else { "lost" });
     return Ok(());
-}
-
-fn prompt() -> u16 {
-    let mut input_line = String::new();
-    print!("best of ");
-    std::io::stdout().flush().unwrap();
-    io::stdin() // the rough equivalent of `std::cin`
-        .read_line(&mut input_line) // actually read the line
-        .expect("Failed to read line"); // which can fail, however
-    let x: u16 = input_line
-        .trim() // ignore whitespace around input
-        .parse() // convert to integers
-        .expect("Input not an integer"); // which, again, can fail
-    return x;
-}
-
-fn cargo() -> Vec<String> {
-    let mut args: Vec<String> = std::env::args().collect();
-    if &args[0] == "target/debug/fear-and-hunger" { args.remove(0); };
-    return args;
 }
