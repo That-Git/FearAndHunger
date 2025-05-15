998adbdc525f3ad6f892e05aa44251ed94c158cd
R <jugle.orgx99@gmail.com>
2025-05-15 12:54:13 +0100

    update build process

--- build.zig
+++ build.zig
@@ -20,8 +20,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "FearAndHunger",
+    // This creates a "module", which represents a collection of source files alongside
+    // some compilation options, such as optimization mode and linked system libraries.
+    // Every executable or library we compile will be based on one or more modules.
+    const lib_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
@@ -29,19 +33,44 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "FearAndHunger",
+    // We will also create a module for our other entry point, 'main.zig'.
+    const exe_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    exe.root_module.addImport("spoon", spoon_mod.module("spoon"));
+    // Modules can depend on one another using the `std.Build.Module.addImport` function.
+    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
+    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
+    exe_mod.addImport("FearAndHunger_lib", lib_mod);
+
+    // Now, we will create a static library based on the module we created above.
+    // This creates a `std.Build.Step.Compile`, which is the build step responsible
+    // for actually invoking the compiler.
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "FearAndHunger",
+        .root_module = lib_mod,
+    });
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
+
+    // This creates another `std.Build.Step.Compile`, but this one builds an executable
+    // rather than a static library.
+    const exe = b.addExecutable(.{
+        .name = "FearAndHunger",
+        .root_module = exe_mod,
+    });
+
+    lib.root_module.addImport("spoon", spoon_mod.module("spoon"));
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -74,17 +103,13 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = exe_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,6 @@
 const std = @import("std");
+const lib = @import("FearAndHunger_lib");
 const mem = std.mem;
-const os = std.posix.system;
-const stdout = std.io.getStdOut().writer();
-
-const spoon = @import("spoon");
-
-var term: spoon.Term = undefined;
-var loop: bool = true;
-
-var cursor: usize = 0;
 
 fn argue() ?bool {
     const a: [][*:0]u8 = std.os.argv;
@@ -27,15 +19,14 @@ fn argue() ?bool {
 pub fn main() void {
     const arg = argue();
     if (arg == null) {
-        select() catch {};
-        term.deinit() catch {};
-        print(0 == cursor % 2);
+        print(0 == lib.position() catch {} % 2);
     } else {
-        print(arg orelse unreachable);
+        print(arg.?);
     }
 }
 
 fn print(face: bool) void {
+    const stdout = std.io.getStdOut().writer();
     const win = std.crypto.random.boolean();
     if (face) {
         stdout.print("heads", .{}) catch {};
@@ -47,117 +38,4 @@ fn print(face: bool) void {
     } else {
         stdout.print(" lost!", .{}) catch {};
     }
-}
-
-fn select() !void {
-    try term.init(.{});
-    defer term.deinit() catch {};
-
-    std.posix.sigaction(os.SIG.WINCH, &os.Sigaction{
-        .handler = .{ .handler = handleSigWinch },
-        .mask = os.empty_sigset,
-        .flags = 0,
-    }, null);
-
-    var fds: [1]os.pollfd = undefined;
-    fds[0] = .{
-        .fd = term.tty.?,
-        .events = os.POLL.IN,
-        .revents = undefined,
-    };
-
-    // zig-spoon will return the terminal back to cooked state automatically
-    // when we call term.deinit().
-    try term.uncook(.{});
-
-    try term.fetchSize();
-    try term.setWindowTitle("feeling luck punk?", .{});
-    try render();
-
-    var buf: [16]u8 = undefined;
-    while (loop) {
-        _ = try std.posix.poll(&fds, -1);
-
-        const read = try term.readInput(&buf);
-        var it = spoon.inputParser(buf[0..read]);
-        while (it.next()) |in| {
-            // The input descriptor parser is not only useful for user-configuration.
-            // Since it can work at comptime, you can use it to simplify the
-            // matching of hardcoded keybinds as well. Down below we specify the
-            // typical keybinds a terminal user would expect for moving up and
-            // down, without getting our hands dirty in the interals of zig-spoons
-            // Input object.
-            if (in.eqlDescription("escape") or in.eqlDescription("q") or in.eqlDescription("enter")) {
-                loop = false;
-                if (!in.eqlDescription("enter")) {
-                    term.deinit() catch {};
-                    std.process.exit(0);
-                }
-                break;
-            } else if (in.eqlDescription("arrow-down") or in.eqlDescription("C-n") or in.eqlDescription("j")) {
-                if (cursor < 1) {
-                    cursor += 1;
-                } else {
-                    cursor -= 1;
-                }
-                try render();
-            } else if (in.eqlDescription("arrow-up") or in.eqlDescription("C-p") or in.eqlDescription("k")) {
-                if (cursor < 1) {
-                    cursor += 1;
-                } else {
-                    cursor -|= 1;
-                }
-                try render();
-            }
-        }
-    }
-}
-
-fn render() !void {
-    var rc = try term.getRenderContext();
-    defer rc.done() catch {};
-
-    try rc.clear();
-
-    if (term.width < 6) {
-        try rc.setAttribute(.{ .fg = .red, .bold = true });
-        try rc.writeAllWrapping("Terminal too small!");
-        return;
-    }
-
-    try rc.moveCursorTo(0, 0);
-    try rc.setAttribute(.{ .fg = .green, .reverse = true });
-
-    // The RestrictedPaddingWriter helps us avoid writing more than the terminal
-    // is wide. It exposes a normal writer interface you can use with any
-    // function that integrates with that, such as print(), write() and writeAll().
-    // The RestrictedPaddingWriter.pad() function will fill the remaining space
-    // with whitespace padding.
-    var rpw = rc.restrictedPaddingWriter(term.width);
-    try rpw.writer().writeAll(" feeling lucky punk?");
-    try rpw.pad();
-
-    try rc.moveCursorTo(1, 0);
-    try rc.setAttribute(.{ .fg = .red, .bold = true });
-    rpw = rc.restrictedPaddingWriter(term.width);
-    try rpw.writer().writeAll(" flipping coins");
-    try rpw.finish(); // No need to pad here, since there is no background.
-
-    const entry_width = @min(term.width - 2, 8);
-    try menuEntry(&rc, " heads", 3, entry_width);
-    try menuEntry(&rc, " tails", 4, entry_width);
-    // try menuEntry(&rc, " →µ←", 6, entry_width);
-}
-
-fn menuEntry(rc: *spoon.Term.RenderContext, name: []const u8, row: usize, width: usize) !void {
-    try rc.moveCursorTo(row, 2);
-    try rc.setAttribute(.{ .fg = .blue, .reverse = (cursor == row - 3) });
-    var rpw = rc.restrictedPaddingWriter(width - 1);
-    defer rpw.pad() catch {};
-    try rpw.writer().writeAll(name);
-}
-
-fn handleSigWinch(_: c_int) callconv(.C) void {
-    term.fetchSize() catch {};
-    render() catch {};
 }
